d6774a8b78c358108c7a92d6d5e0ca31e803097c
owari-taro <taro.biwajima@gmail.com>
2020-04-06 11:12:20 +0900

    request header

--- chapter2/app.py
+++ chapter2/app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+app = Flask(__name__)
 
-
-app=Flask(__nane__)
 
 @app.route("/")
 def index():
     return "<h1>hello world</h1>"
 
-@app.route("/usr/<name>")
+
+@app.route("/user/<name>")
 def user(name):
-    return f"<h1>hello {name}!</h1>"
+    return f"<h1>hello {name}</h1>"  # .format(name)
+
+
+@app.route("/user_agent")
+def user_agent():
+    user_agent = request.headers.get("User-Agent")
+    return f"<p>Your browser is {user_agent}"  # .format(user_agent)
+
+
+if __name__ == "__main__":
+    app.run()
